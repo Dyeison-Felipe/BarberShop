37ecc8f4fbd0f759217f7d4d117708947b8662af
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-10-13 20:46:50 -0300

    chore: Config cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import { AppModule } from './app.module.js';
 import { applyRoutes } from './shared/decorators/http/request-mapping.decorator.js';
 
 function start() {
   const app = express();
   app.use(express.json());
+
+  const corsOptions: cors.CorsOptions = {
+    origin: process.env.FRONTEND_URL, // Allow only this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
+  };
+
+  app.use(cors());
 
   const port = 3333;
 
