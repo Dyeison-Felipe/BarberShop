16a5decd0cf670ecafaff015bb05aa866482ddf2
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-11-07 00:48:45 -0300

    fix: Add asyncLocalStorageService instance to services

--- src/core/barber-shop/modules/barber-shop.module.ts
+++ src/core/barber-shop/modules/barber-shop.module.ts
@@ -1,6 +1,7 @@
 import { BuildModule, IModule } from '../../../shared/modules/module.js';
 import { db } from '../../../shared/repositories/firebase/config.js';
 import { ImageFirebaseStorageService } from '../../../shared/services/image/firestore/image-firebase-storage.service.js';
+import { AsyncLocalStorageService } from '../../../shared/storage-request-service/async-local-storage/async-local-storage-service.js';
 import { BarberShopController } from '../controller/barber-shop.controller.js';
 import { BarberShopFirebaseRepository } from '../repositories/firebase/barber-shop-firebase.repository.js';
 import { BarberShopServiceImpl } from '../service/implementations/barber-shop.service.js';
@@ -9,9 +10,11 @@ export class BarberShopModule implements IModule {
   buildModule(): BuildModule {
     const barberShopRepository = new BarberShopFirebaseRepository(db);
     const imageService = new ImageFirebaseStorageService();
+    const asyncLocalStorageService = new AsyncLocalStorageService();
     const barberShopService = new BarberShopServiceImpl(
       barberShopRepository,
       imageService,
+      asyncLocalStorageService,
     );
     const barberShopController = new BarberShopController(barberShopService);
 
